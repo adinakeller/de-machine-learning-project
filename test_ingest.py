a415d336a97d12453aee2a15503c0c70ed90c6e1
Adina Keller <adinakeller15@gmail.com>
2025-11-25 15:12:51 +0000

    test use of pandas

--- test_ingest.py
+++ test_ingest.py
@@ -1,15 +1,18 @@
-from ingest import load_dataset, save_to_csv
+from ingest import load, save_to_csv
 
 def test_load_dataset(mocker):
     mock_dataset = {
-        'train': {'text': 'im going on a run', 'label': 1},
-        'test': {'text': 'im happy', 'label': 3}
+        'train': [
+            {'text': 'im going on a run', 'label': 1},
+            {'text': 'im happy', 'label': 3}
+            ]
         }
     
     mocker.patch('ingest.load_dataset', return_value=mock_dataset)
-    result = load_dataset('dataset', 'split')
+    result = load(mock_dataset, 'unsplit')
 
-    assert result == {'text': 'im going on a run', 'label': 1}
+    assert len(result) == 2
+    assert result == [{'text': 'im going on a run', 'label': 1}, {'text': 'im happy', 'label': 3}]
 
 def test_saves_csv_to_correct_file():
     data = [
@@ -21,6 +24,8 @@ def test_saves_csv_to_correct_file():
 
     result = save_to_csv(file_name, data)
 
-    assert result == file_name
-
+    assert len(result) == 2
+    assert list(result.columns) == ['text', 'label']
+    assert result.loc[0, 'text'] == 'im going on a run'
+    assert result.loc[1, 'label'] == 3
 
